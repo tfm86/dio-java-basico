18ccbecdd08c4c260a4e098192c164a1d4cccdde
Tiago Fernandes <tfmoraes@gmail.com>
2024-11-17 09:41:16 -0300

    controle

---
+++ controle-teste/src/App1.java
@@ -0,0 +1,10 @@
+public class App1 {
+    public static void main(String[] args) {
+
+        boolean condicao = false;
+        while (condicao) {
+            System.out.println("executou ... ");
+        }
+
+    }
+}
