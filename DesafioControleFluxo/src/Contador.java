ce0507ebe4fedd87b521452e7f85bd922d9ac46f
Tiago Fernandes <tfmoraes@gmail.com>
2024-11-30 00:45:17 -0300

    desafio

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+
+        Scanner terminal = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número:");
+        int primeiro = terminal.nextInt();
+        System.out.println("Informe o segundo número:");
+        int segundo = terminal.nextInt();
+
+        try {
+
+            contar(primeiro, segundo){
+            
+                        }
+            
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
+            
+                }
+
+    private static void contar(int primeiro, int segundo) throws ParametrosInvalidosException {
+
+        if (primeiro < segundo) {
+            int contagem = segundo - primeiro;
+
+            for (int c : contagem) {
+
+            }
+
+        }
+
+        // throw new UnsupportedOperationException("Unimplemented method 'contar'");
+    }
+
+}
